6fbb0555713e54ffdba1d700e6cb67d09edb6ac6
romit111 <57224274+romit111@users.noreply.github.com>
2019-10-31 19:38:24 +0530

    added comment

--- courseCompare-project/courseCompare/urls.py
+++ courseCompare-project/courseCompare/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from course_compare import views
 
+# this file contains list of url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/',include('course_compare.urls')),
